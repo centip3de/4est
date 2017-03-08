17e36c8fad96197cebc2e02d51abc01e86073fa2
Kyle Sammons <kyle@firstfoundry.co>
2017-03-08 15:45:59 -0800

    Added arg parsing removed fizzbuzz

--- interp.py
+++ interp.py
@@ -1,6 +1,7 @@
 import os.path
 import sys
 import traceback
+import argparse
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
@@ -9,11 +10,14 @@ from forest.parser.generated_grammar import ForestParser
 from forest.stack import Stack
 
 class Interpreter():
-    def __init__(self):
+    def __init__(self, debug=False):
         self.stack = Stack()
+        self.debug = debug
 
     def step(self, node):
-        self.stack.print_stack()
+        if self.debug:
+            self.stack.print_stack()
+
         if node[0] == 'D':
             self.stack.push(int(node[1]))
 
@@ -124,8 +128,12 @@ class Interpreter():
             raise Exception("Not a parsable node: " + node)
 
 if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(description="Simple code golfing lang.")
+    arg_parser.add_argument('--debug', action="store_true", help="enables debug mode")
+    args = arg_parser.parse_args()
+
     parser = ForestParser()
-    interp = Interpreter()
+    interp = Interpreter(args.debug)
 
     while True:
         ui = input("> ")
@@ -134,72 +142,16 @@ if __name__ == "__main__":
         else:
             try:
                 ast = parser.parse(Buffer(ui, nameguard=False), rule_name='start')
-                print("AST:", ast)
+
+                if args.debug:
+                    print("AST:", ast)
 
                 for op in ast:
                     interp.step(op)
 
-                interp.stack.print_stack()
+                if args.debug:
+                    interp.stack.print_stack()
 
             except Exception as e:
                 print("ERROR: " + str(e))
                 traceback.print_exc()
-
-"""
-, // Get input
-U // Dup input
-D0 // Push down 0
-= // Compare the input to 0
-! // Negate the comparison
-I // Iterate until the stack is 0
-    O // Pop off that equality
-    U // Duplicate the top of the stack so we don't clober shit
-    D3 // Push down 3 for comparison
-    S // Swap the top two for accurate modulo
-    % // Check if the number is divisible by 3, push result on stack
-    D0 // Push down 0 for comparison
-    = // Pop off the previous 2 and push on equality result
-    ? // If the top of the stack is True
-        O // Pop off the equality result
-        U // Dup the top of the stack so we don't clober
-        D5 // Push down 5 for comparison
-        % // Modulo the last two numbers (check if divisible by 5)
-        S // Swap the top two for accurate modulo
-        D0 // Push down 0 for comparison
-        = // Perform equality
-        ? // If this is also divisible by 5 push "Fizzbuzz"
-            O // Pop off the equality result
-            "FizzBuzz" // Push FizzBuzz to the top of the stack, to be printed if this is divisible by 3 and 5
-        # // If this isn't divisible by 5
-            "Fizz" // If this is divisble only by 3, we need to push "Fizz" to the top of the stack
-        | // Endif
-    # // If this isn't divisible by 3
-        O // Pop off the equality result
-        U // Dup the top of the stack so we don't clober
-        D5 // Push down 5 for comparison
-        S // Swap the top two for accurate modulo
-        % // Modulo the last two numbers (check if divisible by 5)
-        D0 // Push down 0 for comparison
-        = // Perform equality
-        ? // If this is divisible by 5 push "Buzz"
-            O // Pop off the equality result
-            "Buzz"
-        # // Else
-            O // Pop off the equality result
-            // Don't push anything, we want to print the number here if it isn't divisible by 5 or 3
-        | // End if
-    | // End if
-    . // Print the output
-    $ // Remove all but the bottom of the stack (to clear off everything except the "root" number)
-    D1 // Add 1 to subract
-    S // Swap so we don't get a negative
-    - // Perform the subraction
-    U // Dup the result
-    D0 // Push down 0
-    = // See if the current amount is equal to 0
-    ! // Negate the comparison
-E // End loop
-
-Minified version:
-,UD0=!IOUD3S%D0=?OUD5S%D0=?O"FizzBuzz"#"Fizz"|#OUD5S%D0=?O"Buzz"#O||.$D1S-UD0=!E
-"""
